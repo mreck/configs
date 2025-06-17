4a316619407a34cb2bd0ad61a4f347d3da8a08dc
Mathias Reck <mathias.reck@gmail.com>
2025-06-18 00:59:52 +0200

    Add Telescope to Neovim

--- init.lua
+++ init.lua
@@ -1,6 +1,31 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- Leader
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- Setup Plugins
+require("lazy").setup({
+  { 'tpope/vim-fugitive', lazy = true },
+  { 'tpope/vim-sleuth', lazy = true },
+  { 'tpope/vim-surround', lazy = true },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
+})
 
 -- Input
 vim.opt.smartindent = true
@@ -46,3 +71,15 @@ vim.keymap.set({'n'},       'H',           '^')
 vim.keymap.set({'n'},       'L',           '$')
 vim.keymap.set({'n'},       '<C-d>',       '<C-d>zz')
 vim.keymap.set({'n'},       '<C-u>',       '<C-u>zz')
+
+-- Telescope Key Maps
+local telescope = require('telescope.builtin')
+vim.keymap.set({'n'},  '<LEADER>ff',  telescope.find_files)
+vim.keymap.set({'n'},  '<LEADER>fg',  telescope.live_grep)
+vim.keymap.set({'n'},  '<LEADER>fb',  telescope.buffers)
+vim.keymap.set({'n'},  '<LEADER>fh',  telescope.help_tags)
+vim.keymap.set({'n'},  '<LEADER>fm',  telescope.marks)
+vim.keymap.set({'n'},  '<LEADER>fq',  telescope.quickfix)
+vim.keymap.set({'n'},  '<LEADER>fk',  telescope.keymaps)
+vim.keymap.set({'n'},  '<LEADER>/',   telescope.live_grep)
+vim.keymap.set({'n'},  '<LEADER>.',   telescope.git_files)
